71f8b58f47eca6edcbabf2385c6e7a0403a38a09
raaaj1903 <rj471529@gmail.com>
2026-02-04 15:01:02 +0530

    updated

--- DAY-2.sql
+++ DAY-2.sql
@@ -20,11 +20,19 @@ SELECT * FROM employe2;
 INSERT INTO employe2(employe_id,name,position,department,hire_date,salary)
                   VALUES(101,'raj','swe','development','2022-03-13',52000.32),
 				  (102,'rishi','tester','testing','2021-12-15',83322.21),
-				  (103,'pratham','backend dev','development','2023-01-31',15000.23);
+				  (103,'pratham','backend dev','development','2023-01-31',15000.23),
+				  (104,'prathamESH','backend dev','development','2022-01-31',15000.23);
 
 
 -- DELETE COMMAND FOR SPECIFIC ROW
+DELETE FROM employe2
+WHERE  department='testing';
 
- DELETE FROM employe2
- WHERE  department='testing';
+-- Delete employee whose age is greater than 60
+DELETE FROM employees
+WHERE age > 60;
+
+-- Delete employee whose emp_id=101 
+DELETE FROM employees
+WHERE emp_id = 101;
 
